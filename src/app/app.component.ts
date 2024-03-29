fc06c124b664746890554b383a3f5e920a703e30
jorgegp <jorgegp@proton.me>
2024-03-29 01:31:11 +0100

    clean project and add translation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import settings from './app.settings.json';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, TranslateModule],
+  providers: [TranslateService],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'my-roadtrip-planner';
+  constructor(private translateService: TranslateService) {
+    this.translateService.addLangs(settings.availableLanguages);
+    this.translateService.setDefaultLang(settings.defaultLanguage);
+    this.translateService.use(settings.defaultLanguage);
+  }
 }
