5181243723cf33346b314fdacfcade8681c41621
jorgegp <jorgegp@proton.me>
2024-05-31 01:05:02 +0200

    add a map with maptiler

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
-import { GoogleMap } from '@angular/google-maps';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  viewChild,
+} from '@angular/core';
+import { Map, MapStyle, config } from '@maptiler/sdk';
+
+import '@maptiler/sdk/dist/maptiler-sdk.css';
 
 @Component({
   selector: 'rtp-map',
   standalone: true,
-  imports: [GoogleMap],
+  imports: [],
   templateUrl: './map.component.html',
   styleUrl: './map.component.scss',
 })
-export class MapComponent {}
+export class MapComponent implements OnInit, AfterViewInit, OnDestroy {
+  map: Map | undefined;
+
+  private mapContainer = viewChild<ElementRef>('map');
+
+  ngOnInit(): void {
+    config.apiKey = 'WK9Lm5fDxz2LctUOcrzi';
+  }
+
+  ngAfterViewInit() {
+    const initialState = {
+      lat: 36.72,
+      lng: -4.47,
+      zoom: 14,
+    };
+
+    this.map = new Map({
+      container: this.mapContainer().nativeElement,
+      style: MapStyle.STREETS,
+      center: [initialState.lng, initialState.lat],
+      zoom: initialState.zoom,
+    });
+  }
+
+  ngOnDestroy() {
+    this.map?.remove();
+  }
+}
