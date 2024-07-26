9d1919e8ca14bd6c98f2d2c916a8d13b81de355c
jorgegp <jorgegp@proton.me>
2024-07-27 01:12:02 +0200

    fix lint error

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { CardListComponent } from '../../components/card-list/card-list.component';
 import { MapComponent } from '../../components/map/map.component';
-import { cardListMock } from './card-list.mock';
-import { Card } from '../../components/card/card.model';
 
 @Component({
   selector: 'rtp-home',
